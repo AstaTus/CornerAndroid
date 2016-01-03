d660d8fbc395c00390e087c287474de5ed40dbca
AstaTus <chenjunqi5@126.com>
2016-01-03 23:38:40 +0800

    login activity

--- app/src/main/java/com/astatus/cornerandroid/activity/LoginActivity.java
+++ app/src/main/java/com/astatus/cornerandroid/activity/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.astatus.cornerandroid.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 import com.astatus.cornerandroid.R;
 
@@ -11,5 +15,20 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        getSupportActionBar().hide();
+        Button btn = (Button)findViewById(R.id.login_enter_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent it = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(it);
+            }
+        });
+
     }
+
+
+
 }
--- app/src/main/java/com/astatus/cornerandroid/activity/RegisterActivity.java
+++ app/src/main/java/com/astatus/cornerandroid/activity/RegisterActivity.java
@@ -1,7 +1,11 @@
 package com.astatus.cornerandroid.activity;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.astatus.cornerandroid.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        /*Toolbar commonToolbar = (Toolbar) findViewById(R.id.common_toolbar);
+        setSupportActionBar(commonToolbar);*/
+
+        // Get a support ActionBar corresponding to this toolbar
+
+        ActionBar ab = getSupportActionBar();
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
+        ab.setTitle("Register");
     }
 }
